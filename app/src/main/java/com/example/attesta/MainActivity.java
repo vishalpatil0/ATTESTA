59104d339558138a9fc14eecfbcb0fd88c3db459
vishalpatil0 <vishalgpatil10@gmail.com>
2021-12-22 17:30:05 +0530

    Completed half part of the geeksforgeeks video.

--- app/src/main/java/com/example/attesta/MainActivity.java
+++ app/src/main/java/com/example/attesta/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.attesta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button captureBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        captureBtn=findViewById(R.id.idBtnCapture);
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this, ScannerActitivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
