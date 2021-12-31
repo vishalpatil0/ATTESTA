9e947060ed499d0bb29d91d458fb5ef252a7ebfb
asim <asimibushe99@gmail.com>
2021-12-31 17:18:52 +0530

    Added ScrollText activity

---
+++ app/src/main/java/com/example/attesta/ScrollText.java
@@ -0,0 +1,14 @@
+package com.example.attesta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScrollText extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_text);
+    }
+}
