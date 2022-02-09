36238440a5b14a39759540ec885f910ca27ce35f
asim <asimibushe99@gmail.com>
2022-02-09 19:02:36 +0530

    Only adharno extract

--- app/src/main/java/com/example/attesta/ScannerActivity.java
+++ app/src/main/java/com/example/attesta/ScannerActivity.java
@@ -1,17 +1,12 @@
 package com.example.attesta;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import static android.Manifest.permission.CAMERA;
 
 import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.Point;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -20,6 +15,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -27,9 +27,9 @@ import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
-import com.google.mlkit.vision.text.TextRecognizerOptions;
-import android.content.ClipboardManager;
-import static android.Manifest.permission.CAMERA;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.util.List;
 
 
 public class ScannerActivity extends AppCompatActivity {
@@ -131,23 +131,22 @@ public class ScannerActivity extends AppCompatActivity {
         TextRecognizer recognizer= TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
         Task<Text> result =recognizer.process(image).addOnSuccessListener(new OnSuccessListener<Text>() {
             @Override
-            public void onSuccess(@NonNull  Text text) {
-                StringBuilder result= new StringBuilder();
-                for(Text.TextBlock block:text.getTextBlocks()){
-                    String blockText=block.getText();
-                    Point[] blockCornerPoint=block.getCornerPoints();
-                    Rect blockFrame = block.getBoundingBox();
-                    for(Text.Line line:block.getLines()){
-                        String lineTExt=line.getText();
-                        Point[] lineCornerPoint = line.getCornerPoints();
-                        Rect linRect = line.getBoundingBox();
-                        for(Text.Element element: line.getElements()){
-                            String elementText = element.getText();
-                            result.append(elementText);
+            public void onSuccess(@NonNull  Text texts) {
+                List<Text.TextBlock> blocks=texts.getTextBlocks();
+                //resultTV.setText(Integer.toString(blocks.size()));
+                for(int i=0;i<blocks.size();i++)
+                {
+                    List<Text.Line> lines=blocks.get(i).getLines();
+                        for(int j=0;j<lines.size();j++)
+                        {
+                            if(i==2) {
+                                resultTV.setText(lines.get(j).getText());
+                            }
                         }
-                        resultTV.setText(blockText);
-                    }
+
                 }
+
+
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
